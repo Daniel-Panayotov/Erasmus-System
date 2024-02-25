38987db887119cc58b10a0c4124703e7a0ee149a
DoggoFromWater <116671341+Daniel-Panayotov@users.noreply.github.com>
2024-02-25 23:32:10 +0200

    finish admin-home fully responsive page

--- Erasmus-System/src/app/admins/admin-home/admin-home.component.ts
+++ Erasmus-System/src/app/admins/admin-home/admin-home.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-home',
   templateUrl: './admin-home.component.html',
-  styleUrls: ['./admin-home.component.css']
+  styleUrls: ['./admin-home.component.css'],
 })
 export class AdminHomeComponent {
+  isNavShown: boolean = true;
 
+  toggleNav(): void {
+    this.isNavShown = !this.isNavShown;
+  }
 }
--- Erasmus-System/src/app/admins/admins-routing.module.ts
+++ Erasmus-System/src/app/admins/admins-routing.module.ts
@@ -3,7 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { AdminStudentsViewComponent } from './admin-students-view/admin-students-view.component';
 
-const routes: Routes = [{ path: 'home', component: AdminHomeComponent }];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: AdminHomeComponent,
+    children: [{ path: 'students', component: AdminStudentsViewComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
