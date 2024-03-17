9a4aaac4d44e14e1e6c5aa412c935dc4165e416f
DoggoFromWater <116671341+Daniel-Panayotov@users.noreply.github.com>
2024-03-17 23:09:09 +0200

    remove modules

--- Erasmus-System/src/app/app-routing.module.ts
+++ Erasmus-System/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './core/not-found/not-found.component';
+import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     redirectTo: '/home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
   },
   {
     path: 'admins',
--- Erasmus-System/src/app/app.module.ts
+++ Erasmus-System/src/app/app.module.ts
@@ -3,19 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { AdminsModule } from './admins/admins.module';
-import { UsersModule } from './modules/users/users.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    CoreModule,
-    AdminsModule,
-    UsersModule,
-    AppRoutingModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
