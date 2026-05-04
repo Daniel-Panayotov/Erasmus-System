e8a39af58d019f02bbd0871c7c34764d7a093fd6
Daniel Panayotov <dani.dani2004@abv.bg>
2026-05-04 22:27:12 +0300

    feat: add core layout components and styles - footer, header, main

--- APP/src/app/app.routes.ts
+++ APP/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/core/home/home';
+import { NotFound } from './components/core/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home, title: 'Home page' },
+  { path: '**', component: NotFound, title: 'Not found' },
+];
--- APP/src/app/app.ts
+++ APP/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navigation } from './components/core/navigation/navigation';
+import { Footer } from './components/core/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navigation, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('APP');
