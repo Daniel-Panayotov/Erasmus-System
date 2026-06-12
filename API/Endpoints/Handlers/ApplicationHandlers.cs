40aa4d1ef0be1aa9a27f85359424f2ad5740d76b
Daniel Panayotov <dani.dani2004@abv.bg>
2026-06-12 14:43:02 +0300

    fix: remove leftover test code

--- API/Endpoints/Handlers/ApplicationHandlers.cs
+++ API/Endpoints/Handlers/ApplicationHandlers.cs
@@ -2,17 +2,8 @@
 using API.Expressions;
 using API.Models;
 using API.Utilities;
-using Microsoft.AspNetCore.Http.Json;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.Extensions.Primitives;
-using Microsoft.Net.Http.Headers;
-using System.Text.Json;
-using System.Xml.Linq;
-using static System.Collections.Specialized.BitVector32;
-using static System.Net.WebRequestMethods;
 
 namespace API.Endpoints.Handlers;
 
@@ -29,47 +20,6 @@ public class ApplicationHandlers
     }
 
     // TODO: ADD Institution relations when they are implemented
-    public static async Task<IResult> Create2([FromBody] ApplicationDataDTO data, HttpContext http, UEMSContext ctx)
-    {
-        var userID = http.User.TryGetUserID();
-        if (userID == null) return Results.BadRequest("Invalid user identity.");
-
-        var studentQuery = ctx.Students.Where(s => s.UserId == userID);
-        if (!await studentQuery.AnyAsync()) return Results.BadRequest("Invalid user.");
-
-        var student = await studentQuery.Select(StudentExpressions.Base).FirstAsync();
-
-        Application application = new Application
-        {
-            StudentId = student.StudentID,
-            Photo = data.Photo,
-            MobilityType = data.MobilityType.ToString(),
-            StudyFrom = data.StudyFrom,
-            StudyTo = data.StudyTo,
-            Accommodation = data.Accommodation,
-            AccommodationFrom = data.AccommodationFrom,
-            AccommodationTo = data.AccommodationTo,
-            BulgarianCourse = data.BulgarianCourse,
-            MotivationText = data.MotivationText,
-            Degree = data.Degree.ToString(),
-            PriorStudyAbroad = data.PriorStudyAbroad,
-            PriorStudyDuration = data.PriorStudyDuration,
-        };
-        ctx.Applications.Add(application);
-
-        try
-        {
-            var entries = await ctx.SaveChangesAsync();
-            if (entries == 0) return Results.BadRequest("No changes were saved.");
-        }
-        catch (DbUpdateException)
-        {
-            return Results.BadRequest("Database update failed.");
-        }
-
-        return Results.Ok();
-    }
-
     public static async Task<IResult> Create([FromForm] NewApplicationDTO data, HttpContext http, UEMSContext ctx)
     {
         var userID = http.User.TryGetUserID();
