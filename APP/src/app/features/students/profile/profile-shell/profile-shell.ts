3c5ae1507962dc9e6404ec4c2a33859953632fe1
Daniel Panayotov <dani.dani2004@abv.bg>
2026-06-20 01:15:46 +0300

    feat: add angular material and materal tab to the profile shell

--- APP/src/app/features/students/profile/profile-shell/profile-shell.ts
+++ APP/src/app/features/students/profile/profile-shell/profile-shell.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, input } from '@angular/core';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-profile-shell',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatTab, MatTabGroup],
   templateUrl: './profile-shell.html',
   styleUrl: './profile-shell.css',
 })
-export class ProfileShell {}
+export class ProfileShell {
+  private router = inject(Router);
+
+  studentID = input.required<number>();
+
+  navigate(index: number) {
+    if (index == 0) this.router.navigateByUrl(`students/${this.studentID()}/profile/view`);
+    else if (index == 1)
+      this.router.navigateByUrl(`students/${this.studentID()}/profile/language-competencies`);
+    else this.router.navigateByUrl(`students/${this.studentID()}/profile/update`);
+  }
+}
